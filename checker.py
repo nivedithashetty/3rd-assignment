288aa15f2f8ff993c4ecb6aa2371c9973d0ee517
nivedithashetty <shetty.niveditha91@gmail.com>
2013-07-19 16:08:16 +0530

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,3 +1,6 @@
+import random
+global flag
+flag=1
 def find_maxvalue(dummy):
     for j in range(1,n+1):
         q[1][j]=c[1][j]
@@ -16,16 +19,26 @@ def find_maxvalue(dummy):
                 p[i][j]=1
 
 
-def display(i,j):
+def display(i,j,count,tot):
     print j
+    if count!=0:
+        tot=tot+c[count][j]
+        count=count-1
     print("^")
     if i==2:
         print(j+p[i][j])
+        tot=tot+c[count][j+p[i][j]]
+        count=count-1
     else:
-        display(i-1,j+p[i][j])
+        display(i-1,j+p[i][j],count,tot)
+    if flag==1:
+        print "total optimal cost is ",tot
+        global flag
+        flag=0
+    
 
 
-def computeShortestPath(dummy):
+def computeShortestPath(count):
     find_maxvalue(0)
     maxIndex=1
     mx=q[n][1]
@@ -34,12 +47,29 @@ def computeShortestPath(dummy):
             maxIndex=i
             mx=q[n][i]
     print " "
-    display(n,maxIndex)
+    tot=display(n,maxIndex,count,0)
     print "optimal path starts from here "
+    
 
 
-n=5
-c=[[0,0,0,0,0,0,0],[0,9,6,7,8,5,0],[0,5,9,8,7,8,0],[0,8,4,3,9,1,0],[0,6,9,7,1,9,0],[0,5,8,6,15,5,0]]
-q=[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
-p=[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
-computeShortestPath(0)
+
+c=[[0 for i in range(15)] for j in range(15)]
+q=[[0 for i in range(15)] for j in range(15)]
+p=[[0 for i in range(15)] for j in range(15)]
+
+n=int(raw_input("enter the size of the checker board"))
+count=n
+numbers=[1,2,3,4,5,6,7,8,9]
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        c[i][j]=random.choice(numbers)
+
+print"entered board costs are as follows:"
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print c[i][j]
+    print " "
+print " "
+    
+
+computeShortestPath(count)
